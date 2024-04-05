91960303c311c092a8534d19c823599568c8068a
Hamza Zubair <hamza@webevis.com>
2024-04-05 11:09:29 +0500

    initial commit

---
+++ src/pages/_app.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+import Variables from "../styles/variables.css";
+import { HelperClasses, Styling } from "../styles/GlobalStyles.styles";
+
+export default function App({ Component, pageProps }) {
+  const GlobalStyles = createGlobalStyle`
+  ${Variables}
+  ${Styling}
+  ${HelperClasses}
+`;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
