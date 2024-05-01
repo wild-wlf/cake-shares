a82f1ab34935792fae44c9236346581e9815bd16
Hamza Zubair <hamza@webevis.com>
2024-05-01 19:00:12 +0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,7 @@ import Banner from "../components/atoms/banner";
 import Categories from "../components/atoms/categories";
 import CategoriesBar from "../components/atoms/categoriesbar";
 
-export const images = [
-  {
-    image: Property,
-    id: "1",
-  },
-  {
-    id: "2",
-    image: Property2,
-  },
-  {
-    image: Property3,
-    id: "3",
-  },
-  {
-    image: Property,
-    id: "4",
-  },
-  {
-    image: Property2,
-    id: "5",
-  },
-  {
-    image: Property3,
-    id: "6",
-  },
-];
+
 const index = () => {
   return (
     <>
