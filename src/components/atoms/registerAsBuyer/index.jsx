6382d82a9287939abfc4898f1a34c8622bd9c919
Umar Raza <umaarrr0786@gmail.com>
2024-05-21 15:08:25 +0500

    Integrated Login Api

--- src/components/atoms/registerAsBuyer/index.jsx
+++ src/components/atoms/registerAsBuyer/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginSignupModal from "../loginSignupModal";
+import LoginSignupModal from "../buyerloginSignupModal";
 
 const RegisterAsBuyer = ({ handleBuyerModal }) => {
   return <LoginSignupModal handleBuyerModal={handleBuyerModal} />;
