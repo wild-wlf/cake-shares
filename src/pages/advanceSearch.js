c6af1754b2d84c6ba0770d69812cc01320fb6b8a
Hamza Zubair <hamza@webevis.com>
2024-05-31 14:58:56 +0500

    update

--- src/pages/advanceSearch.js
+++ src/pages/advanceSearch.js
@@ -3,6 +3,7 @@ import SearchHeader from "../components/atoms/searchHeader";
 import SearchFilterFields from "../components/atoms/searchFilters";
 import SearchSlider from "../components/atoms/searchSlider";
 import AdvanceSearchGrid from "@/components/atoms/advanceSearchGrid";
+import productService from '@/services/productService';
 
 const AdvanceSearch = () => {
   const [listview, setListView] = useState(true);
@@ -10,6 +11,8 @@ const AdvanceSearch = () => {
   const handleViewController = () => {
     setListView(!listview);
   };
+  const { products_data, products_loading } = productService.GetProducts();
+  console.log(products_data);
   return (
     <>
       <SearchHeader
