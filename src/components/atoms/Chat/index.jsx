a8e7ddc06773c559d8ddccbe0762b25546647493
Umar Raza <umar@webevis.com>
2024-06-05 14:52:56 +0500

    Fixed Chat Layout

--- src/components/atoms/Chat/index.jsx
+++ src/components/atoms/Chat/index.jsx
@@ -1,40 +1,17 @@
 import React from 'react';
-import { ChatBody, ChatFooter, ChatHeader, ChatWrapper } from './Chat.style';
-import Image from 'next/image';
-import Pic from '../../../_assets/seller-img.png';
-import SendIcon from '../../../_assets/send-icon.svg';
-import LinkIcon from '../../../_assets/link-icon.svg';
-import MicIcon from '../../../_assets/mic-icon.svg';
-import PollIcon from '../../../_assets/poll-icon.svg';
-import GalleryIcon from '../../../_assets/gallery-icon.svg';
-const Chat = () => {
+import { ChatBody, ChatWrapper } from './Chat.style';
+
+import ChatHeader from '../ChatHeader';
+import ChatFooter from '../ChatFooter';
+
+const Chat = ({ userInfo }) => {
+  console.log('userInfo', userInfo);
   return (
     <ChatWrapper>
-      <div className="chat-div">
-        <ChatHeader>
-          <Image src={Pic} alt="profilePic" width={40} height={40} />
-          <div>
-            <h6>Logan Paulson</h6>
-            <span>You & Logan Paulson</span>
-          </div>
-        </ChatHeader>
+      <div className="chatWrapper">
+        <ChatHeader userInfo={userInfo} />
         <ChatBody></ChatBody>
-        <ChatFooter>
-          <div className="input-wrapper">
-            <div className="input-div">
-              <Image src={MicIcon} alt="PollIcon" width={14} height={14} />
-              <input placeholder="Type your message..." />
-            </div>
-            <div className="icons-div">
-              <Image src={PollIcon} alt="PollIcon" width={14} height={14} />
-              <Image src={LinkIcon} alt="LinkIcon" width={14} height={14} />
-              <Image src={GalleryIcon} alt="GalleryIcon" width={14} height={14} />
-            </div>
-          </div>
-          <div className="send-icon">
-            <Image src={SendIcon} alt="sendIcon" width={19} height={19} />
-          </div>
-        </ChatFooter>
+        <ChatFooter />
       </div>
     </ChatWrapper>
   );
