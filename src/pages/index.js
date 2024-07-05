727a9b96a4247b3a57ef6467faf94a162aff73a1
Asad Bukhari <asad@plastk.ca>
2024-07-05 16:22:33 +0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import Banner from '../components/atoms/banner';
 import Categories from '../components/atoms/categories';
 import CategoriesBar from '../components/atoms/categoriesbar';
@@ -8,15 +8,11 @@ import Head from 'next/head';
 const Home = () => {
   const [searchQuery, setSearchQuery] = useState({
     page: 1,
-    pageSize: 10,
+    pageSize: 30,
     category: '',
   });
 
-
   const { products_data, products_loading } = productService.GetProducts(searchQuery);
-
-
-  
 
   return (
     <>
@@ -25,7 +21,7 @@ const Home = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Banner />
-      <CategoriesBar  setSearchQuery={setSearchQuery} />
+      <CategoriesBar setSearchQuery={setSearchQuery} />
       <Categories title="Popular Investments" data={products_data?.popularProducts} loading={products_loading} />
       {/* <Categories title="Recommended Investments" data={products_data?.recommendedProducts} loading={products_loading} /> */}
       <Categories title="Advertised Investments" data={products_data?.advertisedProducts} loading={products_loading} />
